abfcf5da6b50ea7683f8ced43e92e17224778a4a
alexmartinssi <alexmbezerrajp@gmail.com>
2018-11-29 12:38:07 -0300

    Criando um FormGroup para controlar o formulário.

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,7 +9,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      this.formGroup = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+        username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+        email: ['', [Validators.required, Validators.email]],
+        password : ['', [Validators.required]],
+     
+      });
   }
 
   signupUser(){
